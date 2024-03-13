68240f5789a1b3a16ac5d34e052778b78db8bc7f
Sahil Ahluwalia <satrader2000@gmail.com>
2024-03-13 22:42:07 +0530

    add: mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { socialMediaLink } from "@/app/contants";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-3 p-24 bg-white text-black font-normal">
+    <main className="flex min-h-screen flex-col gap-3 p-6 sm:p-24 bg-white text-black font-normal">
       <h1 className="text-black">sahilahluwalia.com</h1>
       <h2 className="text-black">
         Hi 👋 My name is Sahil Ahluwalia. I am a Software Developer. I mainly
@@ -29,7 +29,7 @@ export default function Home() {
             href={"https://markmywords.au/"}
           >
             {" "}
-            MarkMyWords
+            MarkMyWordds
           </Link>
           .
         </li>
@@ -52,7 +52,7 @@ export default function Home() {
         I am fortunate to have worked with some amazing people and get to learn
         from them.
       </p>
-      <div className="flex gap-6">
+      <div className="flex gap-6 flex-wrap">
         <Link className="text-blue-600" href={"/blog"}>
           Blog
         </Link>
