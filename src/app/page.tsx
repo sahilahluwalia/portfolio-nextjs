8607a150d79e447257d9d0edfe27861940db7f29
Sahil Ahluwalia <satrader2000@gmail.com>
2024-03-27 13:21:02 +0530

    style: re-ordering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { socialMediaLink } from "@/app/contants";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-3 p-6 sm:p-24 bg-white text-black font-normal">
+    <main className="flex min-h-screen flex-col gap-3 p-6 sm:p-20 bg-white text-black font-normal">
       <h1 className="text-black">sahilahluwalia.com</h1>
       <h2 className="text-black">
         Hi 👋 My name is Sahil Ahluwalia. I am a Software Developer. I mainly
@@ -11,42 +11,42 @@ export default function Home() {
       </h2>
       <p>A little bit about myself:</p>
       <div className="ml-10">
-        <li>wrote first code in 2016</li>
-
-        <li>i am from Mumbai, India.</li>
-        <li>studied Computer Science from 2018 to 2022.</li>
-        <li>
-          i am obsessed about solving problems through code, and trying my best
-          every day.
-        </li>
         <li>
           was full stack developer on AI based Ed-tech platform for australian
           teachers to generate & mark student assessments named{" "}
           <Link
-            rel="noopener noreferrer"
-            target="_blank"
-            className="text-blue-600"
-            href={"https://markmywords.au/"}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="text-blue-600"
+              href={"https://markmywords.au/"}
           >
             {" "}
             MarkMyWords
           </Link>
           .
         </li>
-
         <li>
           was founding software engineer on AI based indian HR platform to ease
           the hiring progress named{" "}
           <Link
-            rel="noopener noreferrer"
-            target="_blank"
-            className="text-blue-600"
-            href={"https://www.workcrew.ai/"}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="text-blue-600"
+              href={"https://www.workcrew.ai/"}
           >
             WorkCrew
           </Link>
           .
         </li>
+        <li>
+          i am obsessed about solving problems through code, and trying my best
+          every day.
+        </li>
+        <li>studied Computer Science from 2018 to 2022.</li>
+        <li>wrote first code in 2016</li>
+        <li>i am from Mumbai, India.</li>
+
+
       </div>
       <p>
         I am fortunate to have worked with some amazing people and get to learn
@@ -58,9 +58,9 @@ export default function Home() {
         </Link>
 
         <Link
-          className="text-blue-600"
-          target="_blank"
-          href={socialMediaLink.linkedIn}
+            className="text-blue-600"
+            target="_blank"
+            href={socialMediaLink.linkedIn}
         >
           Linkedin
         </Link>
